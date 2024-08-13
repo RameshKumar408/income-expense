edf51e8008a9f58437e94cc751690c0afe5d9126
ramesh6435 <ckramesh0006@gmail.com>
2024-08-13 06:13:29 +0530

    change url

--- constant.js
+++ constant.js
@@ -1,7 +1,7 @@
 
 const constant = {
-    // Live_url: "https://income-expense-apps.netlify.app",
-    Live_url: "http://localhost:3000",
+    Live_url: "https://income-expense-apps.netlify.app",
+    // Live_url: "http://localhost:3000",
 }
 
 export default constant;
