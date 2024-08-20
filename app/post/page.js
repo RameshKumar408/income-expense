4a076c0630ca6964f87bb847c3883c3897eeb84d
ramesh6435 <ckramesh0006@gmail.com>
2024-08-20 22:31:03 +0530

    build fix

--- app/post/page.js
+++ app/post/page.js
@@ -14,74 +14,75 @@ export default function post() {
     //     return () => clearInterval(interval);
     // }, []);
 
-    useEffect(() => {
-        // Load Twitter's widget script
-        if (window.twttr) {
-            window.twttr.widgets.load();
-        } else {
-            const script = document.createElement('script');
-            script.src = 'https://platform.twitter.com/widgets.js';
-            script.async = true;
-            document.body.appendChild(script);
-        }
-    }, []);
+    // useEffect(() => {
+    //     // Load Twitter's widget script
+    //     if (window.twttr) {
+    //         window.twttr.widgets.load();
+    //     } else {
+    //         const script = document.createElement('script');
+    //         script.src = 'https://platform.twitter.com/widgets.js';
+    //         script.async = true;
+    //         document.body.appendChild(script);
+    //     }
+    // }, []);
 
     return (
-        <>
-            <div className="twt-cont">
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-                <div className="twitter-container">
-                    <blockquote className="twitter-tweet">
-                        <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
-                    </blockquote>
-                </div>
-            </div>
+        <></>
+        // <>
+        //     <div className="twt-cont">
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //         <div className="twitter-container">
+        //             <blockquote className="twitter-tweet">
+        //                 <a href={`https://twitter.com/user/status/1825448784924688650`}></a>
+        //             </blockquote>
+        //         </div>
+        //     </div>
 
-        </>
+        // </>
     )
 
 }
